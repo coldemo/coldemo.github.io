5d356846adf6ac89be80514aa6e7289988fc3e2e
Fritz Lin <uxfritz@163.com>
2020-04-18 13:36:34 +0800

    Updates

---
+++ code/vue-at.js
@@ -0,0 +1,36 @@
+// await loadJs('https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.min.js')
+await loadJs('https://unpkg.com/vue@2.6.11/dist/vue.min.js')
+
+// await loadJs('https://unpkg.com/vue-at')
+await loadJsForceUmd({
+  url: 'https://unpkg.com/vue-at@2.5.0-beta.2/dist/vue-at.js',
+  name: 'VueAt',
+  deps: { vue: Vue },
+})
+
+appendCss(`
+.container { padding: 20px }
+.editor { padding:4px 8px; height:120px; border:solid 1px gray; white-space:pre-wrap }
+`)
+
+let App = {
+  components: { At: VueAt },
+  template: `
+    <div class="container">
+      <h1>Vue At</h1>
+      <div style="margin-top:20px">
+        <at :members="members" v-model="input">
+          <div class="editor" contenteditable></div>
+        </at>
+      </div>
+    </div>
+  `,
+  data() {
+    return {
+      members: [
+        'fritx', 'linguokang', 'huangruichang'
+      ],
+      input: '@fritx @huangruichang '
+    }
+  }
+}
---
+++ precache-manifest.17308aef6ef2673f96bae30a5aafded2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "742c7b1c4e46a00161d89d5a84fb3554",
+    "url": "./index.html"
+  },
+  {
+    "revision": "78a2363bb2fbfc262954",
+    "url": "./static/css/2.347d920b.chunk.css"
+  },
+  {
+    "revision": "7d97e387b4be33aee02b",
+    "url": "./static/css/main.5cfa1ca8.chunk.css"
+  },
+  {
+    "revision": "78a2363bb2fbfc262954",
+    "url": "./static/js/2.9da51519.chunk.js"
+  },
+  {
+    "revision": "f82f039c0725363f5d159487697915a5",
+    "url": "./static/js/2.9da51519.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d97e387b4be33aee02b",
+    "url": "./static/js/main.2f195e30.chunk.js"
+  },
+  {
+    "revision": "258398776702dc282be1",
+    "url": "./static/js/runtime-main.61a09302.js"
+  }
+]);
