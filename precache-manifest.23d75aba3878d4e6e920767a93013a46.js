cad54cbe5929852369839a1983e128a9ebe341ae
Fritz Lin <uxfritz@163.com>
2020-04-18 15:02:19 +0800

    Updates

---
+++ precache-manifest.23d75aba3878d4e6e920767a93013a46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "244705ba935d69b1dc1dc07fd10fb239",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c3a31b8924ccb9d2f888",
+    "url": "./static/css/2.347d920b.chunk.css"
+  },
+  {
+    "revision": "0b94fbe0e447feb1d9aa",
+    "url": "./static/css/main.d96177f1.chunk.css"
+  },
+  {
+    "revision": "c3a31b8924ccb9d2f888",
+    "url": "./static/js/2.a2458869.chunk.js"
+  },
+  {
+    "revision": "f82f039c0725363f5d159487697915a5",
+    "url": "./static/js/2.a2458869.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b94fbe0e447feb1d9aa",
+    "url": "./static/js/main.d7687050.chunk.js"
+  },
+  {
+    "revision": "258398776702dc282be1",
+    "url": "./static/js/runtime-main.61a09302.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.17308aef6ef2673f96bae30a5aafded2.js"
+  "./precache-manifest.23d75aba3878d4e6e920767a93013a46.js"
 );
 
 self.addEventListener('message', (event) => {
