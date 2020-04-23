ef655b07f7000dc67e06471c5aa8870f093c9687
Fritz Lin <uxfritz@163.com>
2020-04-23 22:29:08 +0800

    Updates

--- precache-manifest.75cbcef155fd00c827be53f958f0aed3.js
+++ precache-manifest.75cbcef155fd00c827be53f958f0aed3.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b6a37f09aa92d39a93651b9cebeef7cd",
+    "revision": "61090a10297327b35bb81eca8d36d0a5",
     "url": "./index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/css/2.347d920b.chunk.css"
   },
   {
-    "revision": "877eeabd93445d55cea7",
+    "revision": "c233df957e91900db301",
     "url": "./static/css/main.9bfd7f55.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.a2458869.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "877eeabd93445d55cea7",
-    "url": "./static/js/main.72ded89c.chunk.js"
+    "revision": "c233df957e91900db301",
+    "url": "./static/js/main.079bb087.chunk.js"
   },
   {
     "revision": "258398776702dc282be1",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9fa6bda6dbdc04bbefc339bdc333f33d.js"
+  "./precache-manifest.75cbcef155fd00c827be53f958f0aed3.js"
 );
 
 self.addEventListener('message', (event) => {
